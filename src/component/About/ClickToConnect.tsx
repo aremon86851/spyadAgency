1c69e76517cd90c925a02fdc0ca96d372db73d3a
aremon86851 <aremon86851@gmail.com>
2023-10-01 14:57:57 +0600

    changes on contact

--- src/component/About/ClickToConnect.tsx
+++ src/component/About/ClickToConnect.tsx
@@ -8,22 +8,22 @@ const ClickToConnect = () => {
     <div className="pl-2">
       <h4 className="text-gray-400 mb-5">
         <a
-          href="https://www.facebook.com/emon.1630/"
+          href="#"
           target="__blank"
           className="flex items-center gap-5 justify-start "
         >
           <img src="/icon/facebook.png" className="w-6" alt="Linkedin icon" />{" "}
-          <span>/emon.1630</span>
+          <span>/spyde</span>
         </a>
       </h4>
       <h4 className="text-gray-400 mb-5">
         <a
-          href="https://www.linkedin.com/in/aremon/"
+          href="#"
           target="__blank"
           className="flex items-center gap-5 justify-start "
         >
           <img src="/icon/linkedin.png" className="w-6" alt="Linkedin icon" />{" "}
-          <span>/in/aremon/</span>
+          <span>/in/spyde/</span>
         </a>
       </h4>
       {/* <h4 className="text-gray-400 mb-5">
@@ -43,26 +43,26 @@ const ClickToConnect = () => {
           className="flex items-center gap-5 justify-start "
         >
           <img src="/icon/whatsapp.png" className="w-6" alt="Github icon" />{" "}
-          <span>+880-16345-71667</span>
+          <span>+880-00000-71667</span>
         </a>
       </h4>
       <h4 className="text-gray-400 mb-5">
         <a
-          href="tel:+880-16345-71667"
+          href="tel:+880-16345-716"
           target="__blank"
           className="flex items-center gap-5 justify-start "
         >
           <img src="/icon/call.svg" className="w-6" alt="Call icon" />{" "}
-          <span>+880-16345-71667</span>
+          <span>+880-00000-71667</span>
         </a>
       </h4>
       <h4 className="text-gray-400 mb-0">
         <a
-          href="mailto:aremon86851@gmail.com"
+          href="mailto:spyde@gmail.com"
           className="flex items-center gap-5 justify-start "
         >
           <img src="/icon/gmail.svg" className="w-6" alt="Gmail icon" />{" "}
-          <span>aremon86851@gmail.com</span>
+          <span>spyde@gmail.com</span>
         </a>
       </h4>
     </div>
