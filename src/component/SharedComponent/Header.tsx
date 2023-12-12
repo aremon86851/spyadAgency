bb9a5367404686f7437977e0bf59d7b15231e428
aremon-coderower <aremon.rahman@coderower.com>
2023-12-12 23:29:49 +0600

    changes pushed

--- src/component/SharedComponent/Header.tsx
+++ src/component/SharedComponent/Header.tsx
@@ -119,7 +119,7 @@ const Header = () => {
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <a
-                  href="#"
+                  href="/"
                   className="block py-2 pl-3 pr-4 text-white rounded md:p-0 "
                   aria-current="page"
                 >
